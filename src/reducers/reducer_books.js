874fc278173f07264c449ae2f4e542ecec418d66
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-05-26 16:47:58 -0300

    Init

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,14 @@
+export default function (){
+    return [
+
+        {title:'Javascript: The Good Parts'},
+
+        {title:'Javascript: The A Parts'},
+
+        {title:'Javascript: The B Parts'},
+
+        {title:'Javascript: The C Parts'}
+    ]
+}
+
+//Redux represents a part of that states.
